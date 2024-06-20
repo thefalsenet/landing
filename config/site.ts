6a52271183ad7d41036e6217d1a6484508763772
Bakhtiyor Ganijon <b.yusupoff001@gmail.com>
2024-06-20 16:19:02 +0900

    init thefalse settings

---
+++ config/site.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+  name: "thefalse",
+  url: "https://thefalse.net",
+  ogImage: "https://thefalse.net/og.jpg",
+  description:
+    "a social network for book lovers. share your reading experience with friends and family. keep track of your reading progress. discover new books and authors.",
+  links: {
+    twitter: "https://twitter.com/thefalsedotnet",
+    github: "https://github.com/",
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
---
+++ lib/font.ts
@@ -0,0 +1,15 @@
+import {
+  JetBrains_Mono as FontMono,
+  Manrope as FontSans,
+} from "next/font/google";
+// import { GeistMono } from "geist/font/mono"
+
+export const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
+
+export const fontMono = FontMono({
+  subsets: ["latin"],
+  variable: "--font-mono",
+});
