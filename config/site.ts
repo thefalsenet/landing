13156473dc8848df041535a7d3bc92d055979555
Bakhtiyor Ganijon <b.yusupoff001@gmail.com>
2025-02-04 14:08:06 +0900

    fix: update Open Graph image extension from .jpg to .jpeg

--- config/site.ts
+++ config/site.ts
@@ -1,7 +1,7 @@
 export const siteConfig = {
   name: "thefalse",
   url: "https://thefalse.net",
-  ogImage: "https://thefalse.net/og.jpg",
+  ogImage: "https://thefalse.net/og.jpeg",
   description:
     "a social network for book lovers. share your reading experience with friends and family. keep track of your reading progress. discover new books and authors.",
   links: {
