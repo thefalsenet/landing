dd432a417a3f844741346a8cf2a17f894f7ce031
Bakhtiyor Ganijon <b.yusupoff001@gmail.com>
2025-09-19 06:06:32 +0900

    fix: add border to CTA section and adjust max-width for background image to enhance layout and visual consistency

--- components/cta-section.tsx
+++ components/cta-section.tsx
@@ -6,7 +6,7 @@ import Footer from "./footer";
 export default function CTASection() {
   return (
     <section className="flex flex-col items-center justify-center mx-auto relative">
-      <div className="flex py-40 px-6 flex-col items-center justify-center gap-12 max-w-[1440px] w-full text-center">
+      <div className="flex py-40 px-6 flex-col items-center justify-center gap-12 max-w-[1440px] border w-full text-center">
         <div className="flex flex-col gap-3 w-full">
           <div>
             {/* <div className="inline-block border p-2 text-sm text-muted-foreground mb-6">
@@ -32,7 +32,7 @@ export default function CTASection() {
           </Button>
         </div>
       </div>
-      <div className="absolute inset-0 z-[-1] w-full overflow-hidden mask-t-from-50% max-w-[1440px]">
+      <div className="absolute inset-0 z-[-1] w-full overflow-hidden max-w-[1440px] mx-auto">
         <Image src={"/cta-bg.png"} className="object-cover object-top grayscale" alt="" fill /></div>
       <Footer className="bg-background/25 backdrop-blur-xl mt-20" />
     </section>
